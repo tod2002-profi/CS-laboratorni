2d98ff9be355e8a06b707f277bb5b23e367e878e
tod2002-profi <56667737+tod2002-profi@users.noreply.github.com>
2020-04-06 12:31:42 +0300

    Add files via upload

---
+++ Лабораторна 10/Laboratorna_10_ex_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratorna_10_ex_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
