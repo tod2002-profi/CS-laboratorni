6d71cafc6b1983aa0d3fae7657018f5fc7f38350
tod2002-profi <56667737+tod2002-profi@users.noreply.github.com>
2020-04-06 12:29:26 +0300

    Add files via upload

---
+++ Лабораторна 9/Laboratorna_9_ex_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Laboratorna_9_ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> basa = new Dictionary<string, string>();
+            string[] basadanih = { "Паспортнi данi", "Освiта", "Спецiальнiсть", "Посада", "Оклад" };
+            foreach (string el in basadanih)
+            {
+                Console.Write($"{el} = ");
+                basa[el] = Convert.ToString(Console.ReadLine());
+            }
+
+            foreach (KeyValuePair<string, string> el in basa)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", el.Key, el.Value);
+            }
+        }
+    }
+}
---
+++ Лабораторна 9/Laboratorna_9_ex_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Laboratorna_9_ex_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> basa = new Dictionary<string, string>();
+            string[] basaigr = { "Назва гри", "Вартість", "Жанр", "Кількістьсть рiвнiв" };
+            foreach (string el in basaigr)
+            {
+                Console.Write($"{el} = ");
+                basa[el] = Convert.ToString(Console.ReadLine());
+            }
+            foreach (KeyValuePair<string, string> el in basa)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", el.Key, el.Value);
+            }
+
+
+        }
+    }
+}
