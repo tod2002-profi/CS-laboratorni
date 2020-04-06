a47018ae2adb212cfeeee2b32f2033d6c576300e
tod2002-profi <56667737+tod2002-profi@users.noreply.github.com>
2020-04-06 17:18:57 +0300

    Add files via upload

---
+++ Лабораторна 11/Laboratorna_11_ex_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Laboratorna_11_ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r1, r2, num;
+            Console.Write("Введіть радіус 1: ");
+            r1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введіть радіус 2: ");
+            r2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введіть число: ");
+            num = Convert.ToDouble(Console.ReadLine());
+            TCirle tcirle = new TCirle(r1, r2);
+            Console.WriteLine("Area: {0}", tcirle.Area());
+            Console.WriteLine("Len circle: {0}", tcirle.Len());
+            Console.WriteLine("r1>r2: {0}", tcirle.Diff());
+            Console.WriteLine("Додавання: {0}", r1 + r2);
+            Console.WriteLine("Віднімання: {0}", r1 - r2);
+            Console.WriteLine("Множення: {0}", r1 * num);
+        }
+    }
+}
---
+++ Лабораторна 11/Laboratorna_11_ex_1/TCircle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Laboratorna_11_ex_1
+{
+    class TCirle
+    {
+        double radius1;
+        double radius2;
+        public TCirle()
+        {
+            radius1 = 5;
+            radius2 = 7;
+        }
+        public TCirle(double radius1, double radius2)
+        {
+            this.radius1 = radius1;
+            this.radius2 = radius2;
+        }
+        public double Area()
+        {
+            return 2 * Math.PI * Math.Pow(radius1, 2);
+        }
+        public double Len()
+        {
+            return 2 * Math.PI * radius1;
+        }
+        public bool Diff()
+        {
+            bool a = false;
+            if (radius1 > radius2)
+                a = true;
+            return a;
+        }
+        public double Value { get; set; }
+        public static TCirle operator +(TCirle radius1, TCirle radius2)
+        {
+
+            return new TCirle { Value = radius1.Value + radius2.Value };
+        }
+        public static TCirle operator -(TCirle radius1, TCirle radius2)
+        {
+
+            return new TCirle { Value = radius1.Value - radius2.Value };
+        }
+        public static TCirle operator *(TCirle radius1, double a)
+        {
+
+            return new TCirle { Value = radius1.Value * a };
+        }
+    }
+}
